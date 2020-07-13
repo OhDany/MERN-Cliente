fed37cc573a1a441cb4d5bbb2e69f1006406206d
Oh Dany <ohdany@Oh-Dany.local>
2020-07-12 23:47:16 -0500

    Formulario de Registro e Inciar sesión

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    // State para iniciar sesión
+    const [usuario, guardarUsuario ] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = usuario;
+
+    const onChange = (e) => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    // Cuando el usuario quiere inciar sesión
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // Validar que no haya campos vacíos
+
+
+        // Pasar al action
+
+    }
+
     return (
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar sesión</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Tu Email"
+                        value={email}
+                        onChange={onChange} />
+                    </div>
+                    
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Tu Password"
+                        value={password}
+                        onChange={onChange} />
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar Seción" />
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener cuenta
+                </Link>
+            </div>
+        </div>
     );
 }
 
